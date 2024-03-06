2ebe7bd003ba37af633b6a13ff4fc5bec4d7c096
Ulises Capistrán <ucapistranmtz@gmail.com>
2024-03-06 15:58:45 -0600

    format

--- main.tf
+++ main.tf
@@ -6,11 +6,11 @@ resource "azurerm_resource_group" "rg" {
 
 
 resource "azurerm_storage_account" "capiapistorage" {
-  name                     = "capiapistorage"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
+  name                          = "capiapistorage"
+  resource_group_name           = azurerm_resource_group.rg.name
+  location                      = azurerm_resource_group.rg.location
+  account_tier                  = "Standard"
+  account_replication_type      = "LRS"
   public_network_access_enabled = false
 }
 
